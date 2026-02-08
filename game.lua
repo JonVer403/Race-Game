2cf6bc44d030611cfa7404bfce27f9bc2da57677
JonVer403 <jcverhoeven@outlook.com>
2026-02-08 18:45:28 +0100

    wedfghjkl

--- game.lua
+++ game.lua
@@ -1,13 +1,18 @@
 local composer = require( "composer" )
 local physics = require("physics")
 local scene = composer.newScene()
+local json = require("json")
+
+local filePath = system.pathForFile("highscore.json", system.DocumentsDirectory)
 
 local score = 0
 local lives = 3
 local baseSpeed = 70
+local Speed = baseSpeed
 local baseTimeToSpawn = 5000
+local enemySpawnTime = baseTimeToSpawn
 local enemyCars = {}
-
+local spawnTimer = nil
 
 
 local borderBottom = display.newRect( display.contentCenterX, display.contentHeight + 200, display.contentWidth, 20 )
@@ -18,18 +23,17 @@ local rightButton = display.newRect( display.contentWidth - 50, display.contentH
 local leftButton = display.newRect( 50, display.contentHeight - 50, 80, 80 )
 
 
-function moveEnemyCar(event)
+function moveEnemyCar()
     local lanes = {display.contentWidth * 0.25, display.contentWidth * 0.5, display.contentWidth * 0.75}
     local laneIndex = math.random(1, 3)
     local enemyX = lanes[laneIndex]
-    local enemyCar = display.newRect( enemyX, -50, 50, 80 )
-    physics.addBody( enemyCar, "dynamic", { isSensor=true } )
-    enemyCar:setLinearVelocity(0, baseSpeed)
-    table.insert(enemyCars, enemyCar)
-    
-    return enemyCar
-end
 
+    local enemyCar = display.newRect(enemyX, -50, 50, 80)
+    physics.addBody(enemyCar, "dynamic", { isSensor=true })
+    enemyCar:setLinearVelocity(0, Speed)
+
+    table.insert(enemyCars, enemyCar)
+end
 
 
 function moveUserCarRight(event)
@@ -49,13 +53,26 @@ function moveUserCarLeft(event)
 end
 
 local function onCollision(event)
-    if event.phase == "began" then
+
+    if (event.phase == "began") then
+
         if lives > 1 then
             lives = lives - 1
             liveText.text = "Lives: " .. lives
         elseif lives == 1 then
             composer.gotoScene("defeat")
         end
+
+        if event.other then
+            for i = #enemyCars, 1, -1 do
+                if (event.other == enemyCars[i]) then
+                    display.remove(enemyCars[i])
+                    table.remove(enemyCars, i)
+                    return
+                end
+            end
+        end
+        
     end
 end
 
@@ -65,12 +82,29 @@ local function scoreUp(event)
     if event.phase == "began" then
         score = score + 1
         scoreText.text = "Score: " .. score
+        if score < 60 then
+            Speed = math.min(baseSpeed + (score * 8), 250)
+            enemySpawnTime = math.max(1000, baseTimeToSpawn - (score * 400))
+        else
+            Speed = 300
+            enemySpawnTime = 800
+        end
+        if spawnTimer then
+            timer.cancel(spawnTimer)
+        end
+
+        spawnTimer = timer.performWithDelay(enemySpawnTime, moveEnemyCar, 0)
+
     end
 end
 
 
+
 function scene:create( event )
     local sceneGroup = self.view
+
+
+    spawnTimer = timer.performWithDelay(enemySpawnTime, moveEnemyCar, 0)
 
     physics.start()
     physics.setGravity(0,0)
@@ -81,22 +115,56 @@ function scene:create( event )
     sceneGroup:insert( userCar )
     sceneGroup:insert( rightButton )
     sceneGroup:insert( leftButton )
-    userCar:addEventListener( "collision", onCollision )
-    borderBottom:addEventListener( "collision", scoreUp )
+    sceneGroup:insert( borderBottom )
     sceneGroup:insert( liveText )
     sceneGroup:insert( scoreText )
 
-    timer.performWithDelay(baseTimeToSpawn, function()
-        local newCar = moveEnemyCar()
-        sceneGroup:insert(newCar)
-    end, 0)
-
+    userCar:addEventListener( "collision", onCollision )
+    borderBottom:addEventListener( "collision", scoreUp )
     rightButton:addEventListener("tap", moveUserCarRight)
     leftButton:addEventListener("tap", moveUserCarLeft)
 end
 
+function scene:show( event )
+    local phase = event.phase
+    
+    if ( phase == "will" ) then
+        -- Reset variables
+        score = 0
+        lives = 3
+        Speed = baseSpeed
+        enemySpawnTime = baseTimeToSpawn
+        scoreText.text = "Score: 0"
+        liveText.text = "Lives: 3"
+        
+    end
+end
+
+function scene:hide( event )
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if ( phase == "will" ) then
+        -- This runs when the scene is about to be removed (e.g., going to 'defeat')
+        
+        -- 1. Cancel the spawn timer
+        if spawnTimer then
+            timer.cancel(spawnTimer)
+            spawnTimer = nil
+        end
+        
+        -- 2. Optional: Stop the physics engine if you want to freeze everything
+        -- physics.pause() 
+    end
+end
+
 function scene:destroy( event )
     local sceneGroup = self.view
+    if spawnTimer then
+        timer.cancel(spawnTimer)
+        spawnTimer = nil
+    end
+    physics.stop()
 
 end
 
